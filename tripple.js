18884e2063176df49303d2cc26ab9eb135d05ba6
coder-utshab <ghoshutshob470@gmail.com>
2026-02-02 22:47:26 +0530

    js

---
+++ tripple.js
@@ -0,0 +1,11 @@
+ //always use ===
+ // equal comparision dees'not work non-primitive 
+ 
+ const first = 2;
+ const secoend = '2';
+ if(first == secoend){
+    console.log('values are equal')
+ }
+ else{
+    console.log('values are not equal')
+ }
