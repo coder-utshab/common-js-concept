62a3320be4e6bd18339e54f6d47d0688307085d9
coder-utshab <ghoshutshob470@gmail.com>
2026-02-02 23:16:38 +0530

    UP

---
+++ hoasting.js
@@ -0,0 +1,17 @@
+
+//  VARIABLE DECLEARATION VAR DEYA KORLA OPOR A NEYA JAY
+
+// FUNCTION AR EXPRASSON DAKLA FUNCTION TA KA NEYA JAY BODY KA NAY NAHH
+
+
+for(let i = 0; i< 5; i++){
+    //console.log(i);
+}
+//console,log('outside',i);
+
+function print5(){
+    console.log('inside print5', 5);
+}
+var print10 = function(){
+    console.log('inside print10'), 10
+}
