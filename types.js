37950fb343892ea6b6e5730064e68263552b6f9a
coder-utshab <ghoshutshob470@gmail.com>
2026-02-02 16:49:16 +0530

    dy

---
+++ types.js
@@ -0,0 +1,17 @@
+//STRONGLY TYPE LANGUAGE
+
+//int a = 5;
+//string b = 'sajib animesh shuvo';
+//bool c = True;
+//object student = { name: 'piyash ha;der', id: 24}
+//int[ numbers = [12, 45, 76]
+//string[] friends = ['piyash', 'apurbo'];
+
+
+//DYNAMIC TYPE LANGUAGE
+ 
+const a = 5;
+const b = 'bangladesh is a small country';
+const ages = [45, 65, 48];
+const student = {id: 23, class: 7}
+console.log(typeof a, typeof b, typeof ages, typeof student);
